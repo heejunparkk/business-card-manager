f77bdc82182fcb2a0ef7d7505d438d41a763cfae
heejun <gmlwns2002@gmail.com>
2025-06-05 00:01:54 +0900

    style: 카드 디테일 페이지 CSS 및 JSX 코드 정리

--- src/app/cards/[id]/page.tsx
+++ src/app/cards/[id]/page.tsx
@@ -335,15 +335,15 @@ export default function CardDetail({ params }: Props) {
                         >
                           {card.address}
                           <span className={styles.naverMapBadge}>
-                            <svg 
-                              xmlns="http://www.w3.org/2000/svg" 
-                              viewBox="0 0 24 24" 
-                              fill="none" 
-                              stroke="currentColor" 
-                              strokeWidth="2" 
-                              strokeLinecap="round" 
-                              strokeLinejoin="round" 
-                              className="h-3 w-3 mr-0.5"
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              viewBox="0 0 24 24"
+                              fill="none"
+                              stroke="currentColor"
+                              strokeWidth="2"
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              className="mr-0.5 h-3 w-3"
                             >
                               <path d="M18 8h1a4 4 0 0 1 0 8h-1"></path>
                               <path d="M2 8h16v9a4 4 0 0 1-4 4H6a4 4 0 0 1-4-4V8z"></path>
